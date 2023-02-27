42464eacd245da46e6aa33ab8b6c3411d95d56c9
Edgar Daniel JaimesH <danieljher21@gmail.com>
2023-02-27 18:27:25 -0400

    Agregando el Proyecto App con sus dependencias

---
+++ app/graficas.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+def generate_bar_chart(name, labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig(f'./img/{name}.png')
+  plt.close()
+
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.savefig('chart_pie.png')
+  plt.close()
+
